51384a68e077bdb9e5c49287398748adfa8bc788
Nick <enviouscodefellow@outlook.com>
2022-10-23 16:31:32 -0700

    initial structure complete

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <main className='main-component'>
+          <p>Hi</p>
+        </main>
+      </>
+    );
+  }
+}
+
+export default Main;
