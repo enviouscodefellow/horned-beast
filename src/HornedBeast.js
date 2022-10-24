1622546f580babc88a144b82c9674a35b3b60b37
Nick <enviouscodefellow@outlook.com>
2022-10-24 15:03:26 -0700

    functional beasts and hearts

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      hearts: 0,
+      favorite: false
+    };
+  };
+
+  handleLove = () => {
+    this.setState({
+    hearts: this.state.hearts + 1
+    });
+  }
+
   render(){
     return(
       <article>
         <h2>{this.props.name}</h2>
-        <img src={this.props.img} alt="Horned Beast" title="Beast Img"/>
+        <p>{this.state.hearts}💓</p>
+        <p onClick={this.handleLove}>Give some love! 💓</p>
+        <img src={this.props.img} alt={this.props.name} title={this.props.desc}/>
+        <p>{this.state.favorite}</p>
         <p>{this.props.desc}</p>
       </article>
 
