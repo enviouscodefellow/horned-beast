a18755041b8bf1bfa60785777bfcd2d4600e98a4
Nick <enviouscodefellow@outlook.com>
2022-11-14 22:03:37 -0700

    modal complete

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import HornedBeast from './HornedBeast.js';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
-  render() {
-    return (
-      <>
-        <main className='main-component'>
-          <HornedBeast name="Test" img="https://www.bing.com/th?id=OP.F6XQ2rS2HMw%2fdw474C474&o=5&pid=21.1&w=140&h=140&qlt=100&dpr=2&c=17" desc='Test Desc'/>
-          <HornedBeast name="Korbin" img="http://www.writeups.org/wp-content/uploads/Korben-Dallas-Fifth-Element-Bruce-Willis-a.jpg"desc='Meat Popsicle'/>
-          <HornedBeast name="Bob"img="https://wallpapercave.com/wp/wp5735633.jpg"desc='Builder'/>
-        </main>
-      </>
-    );
-  }
+    render() {
+        let animals = this.props.data.map((beast, index) => {
+            return <HornedBeast
+                title={beast.title}
+                imageUrl={beast.image_url}
+                description={beast.description}
+                key={beast._id}
+                handleOpenModal={this.props.handleOpenModal}
+            />
+        });
+
+
+        return (
+            <>
+                <Container fluid>
+                    <Row>
+                    {animals}
+                    </Row>
+                </Container>
+            </>
+        )
+    }
 }
 
 export default Main;
